ed4a1798a8da0bbc412228bdf9ed987bdcb29f6f
Martin Bukatovic <mbukatov@redhat.com>
2017-01-20 13:45:20 +0100

    parametrize with one negative

--- test_foobar.py
+++ test_foobar.py
@@ -11,5 +11,10 @@ def test_foo(x, y):
     assert 1 + x - 1 + y != x
 
 
-def test_bar():
-    pass
+@pytest.mark.parametrize("x", [1,2,3]) # positive
+@pytest.mark.parametrize("y", [8,3,7]) # positive
+@pytest.mark.parametrize("z", [None]) # negative
+def test_bar(x, y, z):
+    assert x is not None
+    assert y is not None
+    # assert z is not None
