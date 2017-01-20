1e164057d616f767a7c5a530f8e8790e690b45a1
Martin Bukatovic <mbukatov@redhat.com>
2017-01-20 13:39:03 +0100

    parametrize example

--- test_foobar.py
+++ test_foobar.py
@@ -4,8 +4,11 @@
 import pytest
 
 
-def test_foo():
-    pass
+@pytest.mark.parametrize("x", [1,2,3])
+@pytest.mark.parametrize("y", [8,3,7])
+def test_foo(x, y):
+    assert 1 + x - 1 == x
+    assert 1 + x - 1 + y != x
 
 
 def test_bar():
